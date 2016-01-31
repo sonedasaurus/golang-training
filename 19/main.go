a027eb6434a5386ece18f68de786e88946d94a86
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2016-01-31 16:57:26 +0900

    add Exercise 1.9

---
+++ 19/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	err := fetch(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+}
+
+func fetch(args []string) error {
+	for _, url := range args {
+		if strings.Contains(url, "://") == false {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(os.Stdout, "%s\n", resp.Status)
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
