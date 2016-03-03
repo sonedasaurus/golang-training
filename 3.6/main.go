bcd75f9ab34a345babe2fd4e496b906115b88c9f
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2016-03-03 23:41:59 +0900

    Update ex3.6

--- 3.6/main.go
+++ 3.6/main.go
@@ -16,17 +16,18 @@ func main() {
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
-		y1 := float64(py*2)/height*(ymax-ymin) + ymin
-		y2 := float64(py*2-1)/height*(ymax-ymin) + ymin
+		// 時間があれば、サブピクセル数を指定できるようにする
+		y1 := float64(py*2-1)/(height*2)*(ymax-ymin) + ymin
+		y2 := float64(py*2)/(height*2)*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
-			x1 := float64(px*2)/width*(xmax-xmin) + xmin
-			x2 := float64(px*2-1)/width*(xmax-xmin) + xmin
+			x1 := float64(px*2-1)/(width*2)*(xmax-xmin) + xmin
+			x2 := float64(px*2)/(width*2)*(xmax-xmin) + xmin
 			z1 := complex(x1, y1)
 			z2 := complex(x1, y2)
 			z3 := complex(x2, y1)
 			z4 := complex(x2, y2)
-			grayScale := (mandelbrot(z1) + mandelbrot(z2) + mandelbrot(z3) + mandelbrot(z4)) / 4
-			img.Set(px, py, color.Gray{grayScale})
+			grayScale := (int(mandelbrot(z1)) + int(mandelbrot(z2)) + int(mandelbrot(z3)) + int(mandelbrot(z4))) / 4
+			img.Set(px, py, color.Gray{uint8(grayScale)})
 		}
 	}
 	png.Encode(os.Stdout, img) // NOTE: ignoring errors
@@ -43,5 +44,5 @@ func mandelbrot(z complex128) uint8 {
 			return 255 - (contrast * n)
 		}
 	}
-	return 255
+	return 0
 }
