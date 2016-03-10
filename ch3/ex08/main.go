fa7e6821f7c38560e6f3b8580ed1d7d4485971d8
TakuyaSoneda <soneda@sasanqua>
2016-03-10 21:27:55 +0900

    Add ch3/ex08

---
+++ ch3/ex08/main.go
@@ -0,0 +1,76 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 61.
+//!+
+
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("./ex08 [64|128] > result.png")
+		os.Exit(1)
+	}
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			switch os.Args[1] {
+			case "64":
+				img.Set(px, py, mandelbrot64(complex64(z)))
+			case "128":
+				img.Set(px, py, mandelbrot128(z))
+			default:
+				fmt.Println("./ex08 [64|128]")
+				os.Exit(1)
+			}
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func mandelbrot128(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
+
+func mandelbrot64(z complex64) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex64
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(complex128(v)) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
