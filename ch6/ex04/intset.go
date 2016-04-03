dbdbb94a2879da78995934619cc909657119d206
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2016-04-03 19:46:34 +0900

    Add ch6/ex04

---
+++ ch6/ex04/intset.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type IntSet struct {
+	words []uint64
+}
+
+func main() {
+	var x IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	xSlice := x.CreateSlice()
+	for key, value := range xSlice {
+		fmt.Printf("xSlice[%d] = %d\n", key, value)
+	}
+}
+
+func (s *IntSet) CreateSlice() []int {
+	var slice []int
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				slice = append(slice, 64*i+j)
+			}
+		}
+	}
+	return slice
+}
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
