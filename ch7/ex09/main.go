b5e85361cbaf82b7a3850b28d82188fa0638a9d2
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2016-05-24 00:41:19 +0900

    Update ch7/ex09

--- ch7/ex09/main.go
+++ ch7/ex09/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"html/template"
 	"log"
-	"os"
+	"net/http"
+	"sort"
 	"time"
 )
 
@@ -16,22 +17,23 @@ type Track struct {
 }
 
 type Tracks struct {
-	Tracks []Track
+	Tracks []*Track
+	Keys   []string
 }
 
 var issueList = template.Must(template.New("issuelist").Parse(`
 <h1>Track List</h1>
 <table>
 <tr style='text-align: left'>
-  <th>Title</th>
-  <th>Artist</th>
-  <th>Album</th>
-  <th>Year</th>
-  <th>Length</th>
+  <th><a href="?Title=true">Title</a></th>
+  <th><a href="?Artist=true">Artist</a></th>
+  <th><a href="?Album=true">Album</a></th>
+  <th><a href="?Year=true">Year</a></th>
+  <th><a href="?Length=true">Length</a></th>
 </tr>
   {{range .Tracks}}
 <tr>
-  <td>{{.Title}}</a></td>
+  <td>{{.Title}}</td>
   <td>{{.Artist}}</td>
   <td>{{.Album}}</td>
   <td>{{.Year}}</td>
@@ -50,13 +52,65 @@ func length(s string) time.Duration {
 }
 
 func main() {
-	tracks := Tracks{Tracks: []Track{
+	var tracks = Tracks{Tracks: []*Track{
 		{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
 		{"Go", "Moby", "Moby", 1992, length("3m37s")},
 		{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
 		{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
-	}}
-	if err := issueList.Execute(os.Stdout, tracks); err != nil {
-		log.Fatal(err)
+	}, Keys: nil}
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		if r.FormValue("Title") != "" {
+			tracks.Keys = append(tracks.Keys, "Title")
+		}
+		if r.FormValue("Artist") != "" {
+			tracks.Keys = append(tracks.Keys, "Artist")
+		}
+		if r.FormValue("Album") != "" {
+			tracks.Keys = append(tracks.Keys, "Album")
+		}
+		if r.FormValue("Year") != "" {
+			tracks.Keys = append(tracks.Keys, "Year")
+		}
+		if r.FormValue("Length") != "" {
+			tracks.Keys = append(tracks.Keys, "Length")
+		}
+		sort.Sort(customSort{tracks.Tracks, tracks.Keys, multiTier})
+		if err := issueList.Execute(w, tracks); err != nil {
+			log.Fatal(err)
+		}
 	}
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	return
 }
+
+func multiTier(x, y *Track, sortKeys []string) bool {
+	for _, sortKey := range sortKeys {
+		if sortKey == "Title" && x.Title != y.Title {
+			return x.Title < y.Title
+		}
+		if sortKey == "Artist" && x.Artist != y.Artist {
+			return x.Artist < y.Artist
+		}
+		if sortKey == "Album" && x.Album != y.Album {
+			return x.Album < y.Album
+		}
+		if sortKey == "Year" && x.Year != y.Year {
+			return x.Year < y.Year
+		}
+		if sortKey == "Length" && x.Length != y.Length {
+			return x.Length < y.Length
+		}
+	}
+	return false
+}
+
+type customSort struct {
+	t        []*Track
+	sortKeys []string
+	less     func(x, y *Track, sortKeys []string) bool
+}
+
+func (x customSort) Len() int           { return len(x.t) }
+func (x customSort) Less(i, j int) bool { return x.less(x.t[i], x.t[j], x.sortKeys) }
+func (x customSort) Swap(i, j int)      { x.t[i], x.t[j] = x.t[j], x.t[i] }
