d744d04625c44d40c54d5cb6e46fdc42c3409542
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2016-06-07 08:26:39 +0900

    Update ch7/ex15

--- ch7/ex15/var.go
+++ ch7/ex15/var.go
@@ -2,48 +2,46 @@ package eval
 
 import "fmt"
 
-func (v Var) Var() []string {
-	return []string{v}
+func (v Var) Vars() []string {
+	return []string{string(v)}
 }
 
-func (l literal) Var() []string {
+func (l literal) Vars() []string {
 	return nil
 }
 
-func (u unary) Var() []string {
+func (u unary) Vars() []string {
 	switch u.op {
 	case '+':
-		return u.x.Var()
+		return u.x.Vars()
 	case '-':
-		return u.x.Var()
+		return u.x.Vars()
 	}
 	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
 }
 
-func (b binary) Eval(env Env) []string {
+func (b binary) Vars() []string {
 	switch b.op {
 	case '+':
-		return append(b.x.Var(), b.y.Var()...)
+		return append(b.x.Vars(), b.y.Vars()...)
 	case '-':
-		return append(b.x.Var(), b.y.Var()...)
+		return append(b.x.Vars(), b.y.Vars()...)
 	case '*':
-		return append(b.x.Var(), b.y.Var()...)
+		return append(b.x.Vars(), b.y.Vars()...)
 	case '/':
-		return append(b.x.Var(), b.y.Var()...)
+		return append(b.x.Vars(), b.y.Vars()...)
 	}
 	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
 }
 
-func (c call) Eval(env Env) []string {
+func (c call) Vars() []string {
 	switch c.fn {
 	case "pow":
-		return append(c.args[0].Var(), c.args[0].Var()...)
+		return append(c.args[0].Vars(), c.args[1].Vars()...)
 	case "sin":
-		return c.args[0].Var()
+		return c.args[0].Vars()
 	case "sqrt":
-		return c.args[0].Var()
+		return c.args[0].Vars()
 	}
 	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
 }
-
-//!-Eval2
