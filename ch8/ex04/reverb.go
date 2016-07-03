7f47ca16c74821899afe2f3a45fb9d503db87f13
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2016-07-04 01:58:49 +0900

    Add ch8/ex04

---
+++ ch8/ex04/reverb.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	var wg sync.WaitGroup
+	for input.Scan() {
+		wg.Add(1)
+		go func(c net.Conn, shout string, delay time.Duration) {
+			defer wg.Done()
+			fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+			time.Sleep(delay)
+			fmt.Fprintln(c, "\t", shout)
+			time.Sleep(delay)
+			fmt.Fprintln(c, "\t", strings.ToLower(shout))
+		}(c, input.Text(), 1*time.Second)
+	}
+	go func() {
+		wg.Wait()
+		c.Close()
+	}()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
