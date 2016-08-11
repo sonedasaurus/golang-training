df068d72e48584fbb8d749426b806575c161e856
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2016-08-11 23:23:43 +0900

    wip

---
+++ ch10/ex04/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	out, err := exec.Command("go list ...").Output()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(string(out))
+}
