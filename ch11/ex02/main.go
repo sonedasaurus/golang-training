f7e8c3eed94bc67c7a0d2ef1b3e66de54c28f0be
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2016-08-19 23:41:04 +0900

    fix ch11/ex02

--- ch11/ex02/main.go
+++ ch11/ex02/main.go
@@ -5,62 +5,31 @@ import (
 	"fmt"
 )
 
+type MapIntSet struct {
+	bit map[int]bool
+}
+
+func NewMapIntSet() *MapIntSet {
+	return &MapIntSet{map[int]bool{}}
+}
+
+func (s *MapIntSet) Has(x int) bool {
+	_, bit := s.bit[x]
+	return bit
+}
+
+func (s *MapIntSet) Add(x int) {
+	s.bit[x] = true
+}
+
+func (s *MapIntSet) UnionWith(t *MapIntSet) {
+	for x := range t.bit {
+		s.bit[x] = true
+	}
+}
+
 type IntSet struct {
 	words []uint64
-}
-
-func main() {
-	//!+main
-	var x IntSet
-	x.Add(1)
-	x.Add(144)
-	x.Add(9)
-	fmt.Println(x.String())             // "{1 9 144}"
-	fmt.Printf("Len() = %d\n", x.Len()) // "Len() = 3"
-	x.Remove(1)
-	fmt.Printf("Remove(1) = %s\n", x.String()) // "Remove() = {9 144}"
-	xcopy := x.Copy()
-	xcopy.Add(2)
-	fmt.Printf("x = %s\nxcopy = %s\n", x.String(), xcopy.String()) // "x = {2, 9, 144} xcopy = {9, 144}"
-	x.Clear()
-	fmt.Printf("Clear() = %s\n", x.String()) // "Clear() = {}"
-}
-
-func (s *IntSet) Len() int {
-	count := 0
-	for _, word := range s.words {
-		if word == 0 {
-			continue
-		}
-		for j := 0; j < 64; j++ {
-			if word&(1<<uint(j)) != 0 {
-				count++
-			}
-		}
-	}
-	return count
-}
-
-func (s *IntSet) Remove(x int) {
-	word, bit := x/64, uint(x%64)
-	if word < len(s.words) && s.words[word]&(1<<bit) != 0 {
-		s.words[word] ^= 1 << bit
-	}
-}
-
-func (s *IntSet) Clear() {
-	for i, word := range s.words {
-		s.words[i] ^= word
-	}
-}
-
-func (s *IntSet) Copy() *IntSet {
-	var copy IntSet
-	for i, word := range s.words {
-		copy.words = append(copy.words, 0)
-		copy.words[i] |= word
-	}
-	return &copy
 }
 
 func (s *IntSet) Has(x int) bool {
--- ch11/ex02/main_test.go
+++ ch11/ex02/main_test.go
@@ -1,93 +1,54 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-	"testing"
-)
+import "testing"
 
-func TestLen(t *testing.T) {
+func TestHas(t *testing.T) {
+	intset := intset(0, 63, 64, 127, 128)
+	mapintset := mapintset(0, 63, 64, 127, 128)
 	var tests = []struct {
-		input IntSet
-		want  int
+		input int
 	}{
-		{intset(), 0},
-		{intset(0), 1},
-		{intset(0, 63), 2},
-		{intset(0, 63, 64), 3},
-		{intset(0, 63, 64, 127), 4},
-		{intset(0, 63, 64, 127, 128), 5},
+		{0},
+		{1},
+		{63},
+		{64},
+		{65},
+		{126},
+		{127},
+		{128},
 	}
 
 	for _, test := range tests {
-		fmt.Sprintf("Len()")
-		if test.input.Len() != test.want {
-			t.Errorf("intset.String() is %s", test.input.String())
-			t.Errorf("got: %d", test.input.Len())
-			t.Errorf("want: %d", test.want)
+		if intset.Has(test.input) != mapintset.Has(test.input) {
+			t.Errorf("intset.Has(%d) is %t", test.input, intset.Has(test.input))
+			t.Errorf("mapintset.Has(%d) is %t", test.input, mapintset.Has(test.input))
 		}
 	}
 }
 
-func TestRemove(t *testing.T) {
+func TestAdd(t *testing.T) {
 	var tests = []struct {
-		input  IntSet
-		remove []int
-		want   IntSet
+		input []int
 	}{
-		{intset(), []int{1}, intset()},
-		{intset(2, 64), []int{2}, intset(64)},
-		{intset(2, 64), []int{2, 64}, intset()},
+		{[]int{0}},
+		{[]int{0, 64}},
+		{[]int{0, 65}},
+		{[]int{0, 64, 65}},
+		{[]int{0, 64, 65, 127, 128}},
 	}
 
 	for _, test := range tests {
-		fmt.Sprintf("Remove()")
-		for _, value := range test.remove {
-			test.input.Remove(value)
+		intset := intset()
+		mapintset := mapintset()
+		for _, i := range test.input {
+			intset.Add(i)
+			mapintset.Add(i)
 		}
-		if test.input.String() != test.want.String() {
-			for _, value := range test.remove {
-				t.Errorf("intset.Remove(%d)", value)
+		for _, i := range test.input {
+			if intset.Has(i) == mapintset.Has(i) {
+				t.Errorf("intset.Has(%d) is %t", i, intset.Has(i))
+				t.Errorf("mapintset.Has(%d) is %t", i, mapintset.Has(i))
 			}
-			t.Errorf("got: String() %s", test.input.String())
-			t.Errorf("want: String() %s", test.want.String())
-		}
-	}
-}
-
-func TestClear(t *testing.T) {
-	var tests = []IntSet{
-		intset(),
-		intset(1),
-		intset(2, 64),
-	}
-
-	for _, test := range tests {
-		test.Clear()
-		fmt.Sprintf("Clear()")
-		want := intset()
-		if test.String() != want.String() {
-			t.Errorf("got: String() %s", test.String())
-			t.Errorf("want: String() %s", want.String())
-		}
-	}
-}
-
-func TestCopy(t *testing.T) {
-	var tests = []IntSet{
-		intset(),
-		intset(1),
-		intset(2, 64),
-	}
-
-	for _, test := range tests {
-		fmt.Sprintf("Copy()")
-		got := test.Copy()
-		got.Add(893)
-		if test.String() == got.String() {
-			t.Errorf("copy faild")
-			t.Errorf("original: String() %s", test.String())
-			t.Errorf("copy: String() %s", got.String())
 		}
 	}
 }
@@ -100,12 +61,10 @@ func intset(xs ...int) IntSet {
 	return s
 }
 
-func assertIntSetEquals(t *testing.T, actual, expected IntSet) {
-	if len(actual.words) == 0 && len(expected.words) == 0 {
-		return
+func mapintset(xs ...int) *MapIntSet {
+	s := NewMapIntSet()
+	for _, x := range xs {
+		s.Add(x)
 	}
-
-	if !reflect.DeepEqual(actual.String(), expected.String()) {
-		t.Errorf("got %q\nwant %q", actual.String(), expected.String())
-	}
+	return s
 }
