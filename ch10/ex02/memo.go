ac91c8fd11f72abafe9b2575454c41c9f54c72a4
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2016-08-20 16:21:18 +0900

    Add ch10/ex02

---
+++ ch10/ex02/memo.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+type Func func(key string) (interface{}, error)
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+type entry struct {
+	res   result
+	ready chan struct{} // closed when res is ready
+}
+
+type request struct {
+	key      string
+	response chan<- result // the client wants a single result
+	cancel   chan struct{}
+}
+
+type Memo struct{ requests chan request }
+
+func New(f Func) *Memo {
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f)
+	return memo
+}
+
+func (memo *Memo) Get(key string, canceler chan struct{}) (interface{}, error) {
+	response := make(chan result)
+	memo.requests <- request{key, response, canceler}
+	res := <-response
+	return res.value, res.err
+}
+
+func (memo *Memo) Close() { close(memo.requests) }
+
+func (memo *Memo) server(f Func) {
+	cache := make(map[string]*entry)
+	for req := range memo.requests {
+		e := cache[req.key]
+		if e == nil {
+			// This is the first request for this key.
+			e = &entry{ready: make(chan struct{})}
+			cache[req.key] = e
+			go e.call(f, req.key) // call f(key)
+		}
+		go e.deliver(req.response, req.cancel)
+	}
+}
+
+func (e *entry) call(f Func, key string) {
+	// Evaluate the function.
+	e.res.value, e.res.err = f(key)
+	// Broadcast the ready condition.
+	close(e.ready)
+}
+
+func (e *entry) deliver(response chan<- result, cancel chan struct{}) {
+	select {
+	case <-e.ready:
+		response <- e.res
+	case <-cancel:
+		response <- result{nil, fmt.Errorf("canceled")}
+	}
+}
