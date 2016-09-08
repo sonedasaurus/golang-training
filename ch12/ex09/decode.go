c119715787e7b12d4929f15c56cb912fac98ff62
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2016-09-08 19:44:41 +0900

    Fix ch12/ex09

--- ch12/ex09/decode.go
+++ ch12/ex09/decode.go
@@ -1,153 +1,80 @@
 package sexpr
 
 import (
-	"bytes"
 	"fmt"
-	"reflect"
+	"io"
 	"strconv"
-	"strings"
 	"text/scanner"
 )
 
-var Interfaces map[string]reflect.Type
+type Token interface{}
+type Symbol string
+type String string
+type Int int
+type StartList struct{}
+type EndList struct{}
 
-func Unmarshal(data []byte, out interface{}) (err error) {
-	lex := &lexer{scan: scanner.Scanner{Mode: scanner.GoTokens}}
-	lex.scan.Init(bytes.NewReader(data))
-	lex.next() // get the first token
-	defer func() {
-		// NOTE: this is not an example of ideal error handling.
-		if x := recover(); x != nil {
-			err = fmt.Errorf("error at %s: %v", lex.scan.Position, x)
-		}
-	}()
-	read(lex, reflect.ValueOf(out).Elem())
-	return nil
+func (i Int) String() string {
+	return fmt.Sprintf("Int(%d)", i)
 }
 
-type lexer struct {
+func (s StartList) String() string {
+	return "StartList"
+}
+
+func (s EndList) String() string {
+	return "EndList"
+}
+
+type Decoder struct {
 	scan  scanner.Scanner
-	token rune // the current token
+	err   error
+	depth int
 }
 
-func (lex *lexer) next()        { lex.token = lex.scan.Scan() }
-func (lex *lexer) text() string { return lex.scan.TokenText() }
+func NewDecoder(r io.Reader) *Decoder {
+	var scan scanner.Scanner
+	scan.Init(r)
+	dec := Decoder{scan: scan}
+	scan.Error = dec.setError
+	return &dec
+}
 
-func (lex *lexer) consume(want rune) {
-	if lex.token != want { // NOTE: Not an example of good error handling.
-		panic(fmt.Sprintf("got %q, want %q", lex.text(), want))
+func (d *Decoder) setError(scan *scanner.Scanner, msg string) {
+	d.err = fmt.Errorf("scanning: %s", msg)
+}
+
+func (d *Decoder) Token() (Token, error) {
+	t := d.scan.Scan()
+	if d.err != nil {
+		return nil, d.err
 	}
-	lex.next()
-}
-
-func read(lex *lexer, v reflect.Value) {
-	switch lex.token {
-	case scanner.Ident:
-		switch lex.text() {
-		case "nil":
-			v.Set(reflect.Zero(v.Type()))
-			lex.next()
-			return
-		case "t":
-			v.SetBool(true)
-			lex.next()
-			return
-		}
-	case scanner.String:
-		s, _ := strconv.Unquote(lex.text()) // NOTE: ignoring errors
-		v.SetString(s)
-		lex.next()
-		return
-	case scanner.Int:
-		i, _ := strconv.Atoi(lex.text()) // NOTE: ignoring errors
-		switch v.Type().Kind() {
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			v.SetInt(int64(i))
-			lex.next()
-			return
-		case reflect.Float32, reflect.Float64:
-			v.SetFloat(float64(i))
-			lex.next()
-			return
-		}
-	case scanner.Float:
-		f, _ := strconv.ParseFloat(lex.text(), 64) // NOTE: ignoring errors
-		v.SetFloat(float64(f))
-		lex.next()
-		return
-	case '(':
-		lex.next()
-		readList(lex, v)
-		lex.next() // consume ')'
-		return
+	if d.depth == 0 && t != '(' && t != scanner.EOF {
+		return nil, fmt.Errorf("expecting '(', got %s", scanner.TokenString(t))
 	}
-	panic(fmt.Sprintf("unexpected token %q", lex.text()))
-}
-
-func readList(lex *lexer, v reflect.Value) {
-	switch v.Kind() {
-	case reflect.Array: // (item ...)
-		for i := 0; !endList(lex); i++ {
-			read(lex, v.Index(i))
-		}
-
-	case reflect.Slice: // (item ...)
-		for !endList(lex) {
-			item := reflect.New(v.Type().Elem()).Elem()
-			read(lex, item)
-			v.Set(reflect.Append(v, item))
-		}
-
-	case reflect.Struct: // ((name value) ...)
-		for !endList(lex) {
-			lex.consume('(')
-			if lex.token != scanner.Ident {
-				panic(fmt.Sprintf("got token %q, want field name", lex.text()))
-			}
-			name := lex.text()
-			lex.next()
-			read(lex, v.FieldByName(name))
-			lex.consume(')')
-		}
-
-	case reflect.Map: // ((key value) ...)
-		v.Set(reflect.MakeMap(v.Type()))
-		for !endList(lex) {
-			lex.consume('(')
-			key := reflect.New(v.Type().Key()).Elem()
-			read(lex, key)
-			value := reflect.New(v.Type().Elem()).Elem()
-			read(lex, value)
-			v.SetMapIndex(key, value)
-			lex.consume(')')
-		}
-
-	case reflect.Interface: // (name value)
-		name := strings.Trim(lex.text(), `"`)
-		lex.next()
-		typ, ok := Interfaces[name]
-		if !ok {
-			panic(fmt.Sprintf("no concrete type registered for interface %s", name))
-		}
-		val := reflect.New(typ)
-		read(lex, reflect.Indirect(val))
-		v.Set(reflect.Indirect(val))
-
-	default:
-		panic(fmt.Sprintf("cannot decode list into %v", v.Type()))
-	}
-}
-
-func endList(lex *lexer) bool {
-	switch lex.token {
+	switch t {
 	case scanner.EOF:
-		panic("end of file")
+		return nil, io.EOF
+	case scanner.Ident:
+		return Symbol(d.scan.TokenText()), nil
+	case scanner.String:
+		text := d.scan.TokenText()
+		// Assume all strings are quoted.
+		return String(text[1 : len(text)-1]), nil
+	case scanner.Int:
+		n, err := strconv.ParseInt(d.scan.TokenText(), 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		return Int(n), nil
+	case '(':
+		d.depth++
+		return StartList{}, nil
 	case ')':
-		return true
+		d.depth--
+		return EndList{}, nil
+	default:
+		pos := d.scan.Pos()
+		return nil, fmt.Errorf("unexpected token %s at L%d:C%d", scanner.TokenString(t), pos.Line, pos.Column)
 	}
-	return false
-}
-
-func init() {
-	Interfaces = make(map[string]reflect.Type)
 }
